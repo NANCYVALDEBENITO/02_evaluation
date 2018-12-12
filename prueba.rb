cb4d93f55d88652e12fa1f2a6d00a67f028f9e82
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-12-12 19:35:17 -0300

    add csv

--- prueba.rb
+++ prueba.rb
@@ -21,7 +21,6 @@ loop do
 	puts '2 Ingresar el nombre de un producto y buscar el stock'
 	puts '3 Conocer los productos registrados en bodega'
 	puts '4 Conocer los productos con existencia total menor a un valor'
-	puts '5 Registra un nuevo producto con el stock'
 	puts '6 Salir'
 	select = gets.chomp
 
@@ -34,9 +33,7 @@ loop do
 		item3
 	elsif select == '4'
 		puts ''
-	elsif select == '5'
-		item5
-	elsif select == '6'
+	elsif select == 'exit'
 		break
 	else
 		puts 'Esta no es una opción válida'
