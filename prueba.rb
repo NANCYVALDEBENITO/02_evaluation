2a82535cea13eac83dd8a4beb3fc8f27b74ed6aa
nvaldebenito <nvaldebenito@localhost.localdomain>
2018-12-12 21:01:37 -0300

    build and read hash ok

--- prueba.rb
+++ prueba.rb
@@ -1,42 +1,58 @@
-def item1
-	
+def readcsv_to_hash
+  # import requires for opening a csv file
+  require 'csv'
+  # create array and hash for operating
+  data_hash = {}
+  names = []
+  califications = []
+  # open csv for reading data
+  CSV.foreach('file.csv') do |row|
+    names.push(row.shift)
+    califications.push(row)
+  end
+  # append array to hash
+  data_hash[:name] = names
+  data_hash[:calification] = califications
+  data_hash
 end
+
+def item1(name_str)
+  h = readcsv_to_hash
+  puts h[:name].length
+  (0..h[:name].length - 1).each do |n|
+    puts h[:name][n] if h[:name][n] == name_str
+  end
+end
+
+item1('David')
 
 def item2
-
-end
-def item2
-
-end
-def item2
-
-end
-def item2
-
+  puts ''
 end
 
-loop do
-	puts 'Ingrese una opción'
-	puts '1 Conocer la cantidad de productos existentes'
-	puts '2 Ingresar el nombre de un producto y buscar el stock'
-	puts '3 Conocer los productos registrados en bodega'
-	puts '4 Conocer los productos con existencia total menor a un valor'
-	puts '6 Salir'
-	select = gets.chomp
-
-	if select == '1'
-		item1
-	elsif select == '2'
-		puts ''
-	elsif select == '3'
-		puts 'Los productos disponibles son :' 
-		item3
-	elsif select == '4'
-		puts ''
-	elsif select == 'exit'
-		break
-	else
-		puts 'Esta no es una opción válida'
-		
-	end
+def item3
+  puts ''
 end
+
+def item4
+  puts ''
+end
+
+# loop do
+
+#   if select == '1'
+#     item1
+#   elsif select == '2'
+#     item2
+#   elsif select == '3'
+#     puts 'Los productos disponibles son :'
+#     item3
+#   elsif select == '4'
+#     puts ''
+#   elsif select == 'exit'
+#     break
+#   else
+#     puts 'Esta no es una opción válida'
+
+#   end
+# end
